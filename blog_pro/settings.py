4a332d9ee4acb01dc67b8c0444fbc4056ed737a1
Marcus Morrow <marcusmorrow319@gmail.com>
2021-04-11 14:56:18 -0500

    added styling to search and listings page

--- blog_pro/settings.py
+++ blog_pro/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'blog_pro/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'blog_pro/static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
